589ed0bcfa7e61041d29be0f1cd1ad44d7dfc24e
Lauren Weston <lauren.weston@cantarus.com>
2020-02-09 12:20:54 +0000

    Add all the files I've been writing
    I had source control issues, wrong folder..

---
+++ StringCalculator.test/CalculatorTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData("", 0)]
+        public void Add_EmptyString_ReturnsZero(string input, int expected)
+        {
+            // Arrange
+            var calc = new Calculator();
+            // Act
+            var result = calc.Add(input);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("0", 0)]
+        public void Add_OneNumber_ReturnsNumber(string input, int expected)
+        {
+            var calc = new Calculator();
+            calc.Add(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("1,1", 2)]
+        [InlineData("1,2", 3)]
+        [InlineData("0,2", 2)]
+        public void Add_TwoNumbers_ReturnsSum(string input, int expected)
+        {
+            var calc = new Calculator();
+            calc.Add(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("1,2,3,4", 10)]
+        [InlineData("1,2,3,4,0", 10)]
+        public void Add_AnyAmountOfNumbers(string input, int expected)
+        {
+            var calc = new Calculator();
+            calc.Add(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("1\n2", 3)]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("1,2\n3\n4", 10)]
+        public void Add_AllowCommaAndNewLineDelimiters(string input, int expected)
+        {
+            var calc = new Calculator();
+            calc.Add(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("//;\n1;2", 3)]
+        [InlineData("//~\n1~2~3", 6)]
+        public void Add_SpecifyDelimiter(string input, int expected)
+        {
+            var calc = new Calculator();
+            calc.Add(input).Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("-1", "Negatives not allowed: -1")]
+        [InlineData("1,-2", "Negatives not allowed: -2")]
+        [InlineData("1,-2, -3", "Negatives not allowed: -2, -3")]
+        public void Add_Negatives_ThrowException(string input, string expected)
+        {
+            var calc = new Calculator();
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() => calc.Add(input));
+
+            ex.Message.Should().Be(expected);
+        }
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public int Add(string input)
+        {
+            if (input == "")
+                return 0;
+
+            if (input.Length == 1)
+            {
+                int.TryParse(input, out int number);
+                if (number < 0)
+                {
+                    string message = "Negatives not allowed: {0}";
+                    message = string.Format(message, number);
+                    throw new ArgumentOutOfRangeException(message);
+                }
+                return int.Parse(input);
+            }
+            char[] delimiters = new char[] { ',', '\n' };
+            string values = input;
+
+            if("" + input[0] + input[1] == "//")
+            { 
+                string[] lines = input.Split('\n');
+                var firstLine = lines[0];
+                values = lines[1];
+
+                char delimiter = firstLine.ToCharArray(2, 1)[0];
+                delimiters = new char[] { delimiters[0], delimiters[1], delimiter };
+            }
+
+            string[] numbers = values.Split(delimiters);
+
+            int sum = 0;
+            foreach(string number in numbers)
+            {
+                sum += int.Parse(number);
+            }
+            return sum;
+        }
+    }
+
+}

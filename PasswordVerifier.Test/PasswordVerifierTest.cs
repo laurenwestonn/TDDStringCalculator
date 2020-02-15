b86ff37c8a5bd957b7c48f9661caf81c01aa58f4
Lauren Weston <lauren.weston@cantarus.com>
2020-02-15 22:31:33 +0000

    Failing test: Exception test

--- PasswordVerifier.Test/PasswordVerifierTest.cs
+++ PasswordVerifier.Test/PasswordVerifierTest.cs
@@ -105,7 +105,23 @@ namespace PasswordVerifier.Test
             );
         }
 
-
+        [TestMethod]
+        [DataRow("1234567")]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TooShort_Exception(string password)
+        {
+            new PasswordVerifier(
+                new CombineVerifiers(
+                    new IVerifier[] {
+                        new NoLongerThanVerifier(8),
+                        new NotNullVerifier(),
+                        new AtLeastOneLowerCaseVerifier(),
+                        new AtLeastOneUpperCaseVerifier(),
+                        new AtLeastOneNumberVerifier()
+                    }
+                )
+            ).Verify(password);
+        }
 
 
     }

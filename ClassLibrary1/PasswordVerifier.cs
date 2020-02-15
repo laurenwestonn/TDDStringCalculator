6059b956ba16d828ff5fa19957d75efaf0b52e94
Lauren Weston <lauren.weston@cantarus.com>
2020-02-15 17:45:24 +0000

    Extract classes to test individually

--- ClassLibrary1/PasswordVerifier.cs
+++ ClassLibrary1/PasswordVerifier.cs
@@ -6,14 +6,54 @@ using System.Threading.Tasks;
 
 namespace PasswordVerifier
 {
-    public static class PasswordVerifier
+
+    public class PasswordVerifier
     {
-        public static bool Verify(string password)
+        private readonly IVerifier verifier;
+
+        public PasswordVerifier(IVerifier _verifier)
         {
-            if (password != null && password.Length > 8)
-                return true;
-            else
-                return false;
+            verifier = _verifier;
+        }
+
+        public bool Verify(string input)
+        {
+            return verifier.Verify(input);
+        }
+    }
+    
+
+    public interface IVerifier
+    {
+        bool Verify(string input);
+    }
+
+    public class NoLongerThanVerifier: IVerifier
+    {
+        readonly int length;
+        public NoLongerThanVerifier(int _length)
+        {
+            length = _length;
+        }
+        public bool Verify(string input)
+        {
+            return input.Length > length;
+        }
+    }
+
+    public class NotNullVerifier: IVerifier
+    {
+        public bool Verify(string input)
+        {
+            return input != null;
+        }
+    }
+
+    public class AtLeastOneUpperCaseVerifier: IVerifier
+    {
+        public bool Verify(string input)
+        {
+            return input.ToLower() != input;
         }
     }
 }
--- PasswordVerifier.Test/PasswordVerifierTest.cs
+++ PasswordVerifier.Test/PasswordVerifierTest.cs
@@ -8,36 +8,30 @@ namespace PasswordVerifier.Test
     {
         [TestMethod]
         [DataRow("12345678", false)]
+        [DataRow("123456789", true)]
         public void MustBeLargerThan8Chars(string password, bool expected)
         {
-            Assert.AreEqual(PasswordVerifier.Verify(password), expected);
+            Assert.AreEqual(new NoLongerThanVerifier(8).Verify(password), expected);
         }
 
         [TestMethod]
         [DataRow(null, false)]
+        [DataRow("", true)]
         public void MustNotBeNull(string password, bool expected)
         {
-            Assert.AreEqual(PasswordVerifier.Verify(password), expected);
+            Assert.AreEqual(new NotNullVerifier().Verify(password), expected);
         }
 
-        // This new rule makes the test for the first one break
-        // 12345678 would pass. Now it would fail as no uppercase letters..
-        // We can't test for positives (i.e. if rule met, pw verified)
-        // as the verification relies on many rules
-        // We can only test for fails (only 6 characters should always fail
-        // regardless of other rules)
-        // BUT! We can't be sure of which rule failed it.
-        // e.g. "dfgb" would break this rule, but the code would actually fail
-        // that password based on the first rule, so it wouldn't be a test for this rule
-        //
-        // The only way I can think of testing this is to test each rule separately
-        // by extracting each into separate classes + calling them all within Verify()
-        // But this sounds mighty inefficient
         [TestMethod]
         [DataRow("123kjh", false)]
+        [DataRow("123kj33hffgfg", false)]
+        [DataRow("A123kjh", true)]
+        [DataRow("123fgfA", true)]
+        [DataRow("12A3fRgfg", true)]
+        [DataRow("qwertY", true)]
         public void AtLeastOneUpperCase(string password, bool expected)
         {
-            Assert.AreEqual(PasswordVerifier.Verify(password), expected);
+            Assert.AreEqual(new AtLeastOneUpperCaseVerifier().Verify(password), expected);
         }
     }
 }

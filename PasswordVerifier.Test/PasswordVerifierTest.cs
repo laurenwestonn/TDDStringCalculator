f7168c232b4fccb41cd866fdaa9686123d310c9d
Lauren Weston <lauren.weston@cantarus.com>
2020-02-15 14:49:01 +0000

    Test for uppercase char raised some concerns
    as mentioned in the comment.
    
    I don't think I can test this, so i'll end here

--- PasswordVerifier.Test/PasswordVerifierTest.cs
+++ PasswordVerifier.Test/PasswordVerifierTest.cs
@@ -8,7 +8,6 @@ namespace PasswordVerifier.Test
     {
         [TestMethod]
         [DataRow("12345678", false)]
-        [DataRow("123456789", true)]
         public void MustBeLargerThan8Chars(string password, bool expected)
         {
             Assert.AreEqual(PasswordVerifier.Verify(password), expected);
@@ -17,6 +16,26 @@ namespace PasswordVerifier.Test
         [TestMethod]
         [DataRow(null, false)]
         public void MustNotBeNull(string password, bool expected)
+        {
+            Assert.AreEqual(PasswordVerifier.Verify(password), expected);
+        }
+
+        // This new rule makes the test for the first one break
+        // 12345678 would pass. Now it would fail as no uppercase letters..
+        // We can't test for positives (i.e. if rule met, pw verified)
+        // as the verification relies on many rules
+        // We can only test for fails (only 6 characters should always fail
+        // regardless of other rules)
+        // BUT! We can't be sure of which rule failed it.
+        // e.g. "dfgb" would break this rule, but the code would actually fail
+        // that password based on the first rule, so it wouldn't be a test for this rule
+        //
+        // The only way I can think of testing this is to test each rule separately
+        // by extracting each into separate classes + calling them all within Verify()
+        // But this sounds mighty inefficient
+        [TestMethod]
+        [DataRow("123kjh", false)]
+        public void AtLeastOneUpperCase(string password, bool expected)
         {
             Assert.AreEqual(PasswordVerifier.Verify(password), expected);
         }

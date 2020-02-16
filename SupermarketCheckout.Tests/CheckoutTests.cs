e4b6ba033e971df3d407c836a877824b290aad54
Lauren Weston <lauren.weston@cantarus.com>
2020-02-16 12:12:56 +0000

    Failing test

--- SupermarketCheckout.Tests/CheckoutTests.cs
+++ SupermarketCheckout.Tests/CheckoutTests.cs
@@ -12,5 +12,14 @@ namespace SupermarketCheckout.Tests
             c.Scan("A");
             Assert.AreEqual(15, c.Total);
         }
+        [TestMethod]
+        public void ScanningMultipleItems_AddsToTotal()
+        {
+            Checkout c = new Checkout();
+            c.Scan("A");
+            c.Scan("A");
+            c.Scan("A");
+            Assert.AreEqual(45, c.Total);
+        }
     }
 }
